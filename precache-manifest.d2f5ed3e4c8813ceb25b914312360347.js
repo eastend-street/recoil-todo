5253ea904eb775ba5a3d56cecf5b6d38597ab480
eastend-street <suminari.yamada@gmail.com>
2020-05-17 03:14:42 -0700

    Updates

---
+++ precache-manifest.d2f5ed3e4c8813ceb25b914312360347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bc4e6e15350e5ae721a246d0800823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "673e5cbdab4cf5e37506",
+    "url": "/static/css/main.bb37429d.chunk.css"
+  },
+  {
+    "revision": "1946c328ccd127036555",
+    "url": "/static/js/2.a78158cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a78158cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673e5cbdab4cf5e37506",
+    "url": "/static/js/main.7cc22aed.chunk.js"
+  },
+  {
+    "revision": "29966a30f933283a2e52",
+    "url": "/static/js/runtime-main.4336cbf1.js"
+  }
+]);
