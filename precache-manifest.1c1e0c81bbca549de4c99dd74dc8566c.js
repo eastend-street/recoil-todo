486eb5cd6a5a0715b926a260e55d7dab9cad1713
eastend-street <suminari.yamada@gmail.com>
2020-05-17 03:18:49 -0700

    Updates

---
+++ precache-manifest.1c1e0c81bbca549de4c99dd74dc8566c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17c5dd0c966a73bdfa0ce9bdfaa667f",
+    "url": "/recoil-todo/index.html"
+  },
+  {
+    "revision": "e5eab7f887e5387e210c",
+    "url": "/recoil-todo/static/css/main.bb37429d.chunk.css"
+  },
+  {
+    "revision": "8fb50740c1e2f2d512cc",
+    "url": "/recoil-todo/static/js/2.e8aaa69d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recoil-todo/static/js/2.e8aaa69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5eab7f887e5387e210c",
+    "url": "/recoil-todo/static/js/main.509cdf85.chunk.js"
+  },
+  {
+    "revision": "c02cad6d3854a7b3ee82",
+    "url": "/recoil-todo/static/js/runtime-main.2ac90072.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2f5ed3e4c8813ceb25b914312360347.js"
+  "/recoil-todo/precache-manifest.1c1e0c81bbca549de4c99dd74dc8566c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recoil-todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
